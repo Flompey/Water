6b2f81235b6cd9ef9c4e549203e728d62d71c32b
Flompey <34039955+Flompey@users.noreply.github.com>
2020-11-13 14:25:23 +0100

    Added precompiled header

---
+++ Water/Source/PrecompiledHeader.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <memory>
+#include <iterator>
+
+// Data structures
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+
