630b2dfa5d681cbfd40c1b75d2c355ab4d80d110
Flompey <34039955+Flompey@users.noreply.github.com>
2020-12-10 15:31:35 +0100

    Implemented matrix class

---
+++ Water/Source/Mathematics/Algorithms.h
@@ -0,0 +1,22 @@
+#pragma once
+
+template<class T>
+requires(std::is_floating_point_v<T>)
+std::string ConvertFloatingPointToRoundedString(const T value, const size_t decimalCount)
+{
+	std::string string = std::to_string(value);
+	const size_t decimalPointPos = string.find('.');
+	if (decimalPointPos != std::string::npos)
+	{
+		string = string.substr(0, decimalPointPos + decimalCount + 1);
+	}
+	return string;
+}
+
+template<class T>
+requires(std::is_floating_point_v<T>)
+T Round(const T value, const size_t decimalCount)
+{
+	T adjuster = (T)std::pow(10.0, (double)decimalCount);
+	return std::floor(value * adjuster + (T)0.5) / adjuster;
+}
