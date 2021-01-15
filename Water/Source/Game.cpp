f60a6bbb4a4b2c26a720589d5b7d4489fde9a0e7
Flompey <34039955+Flompey@users.noreply.github.com>
2021-01-15 18:17:06 +0100

    We are now rendering water and testing out some post-processing effects.

--- Water/Source/Game.cpp
+++ Water/Source/Game.cpp
@@ -1,19 +1,36 @@
 #include "Game.h"
 #include "Benchmark/BenchmarkMacros.h"
+#include "Rendering/GlMacro.h"
+#include "Console/Log.h"
 
 Game::Game()
-	:
+    :
     mWindow("Water", 1920, 1080),
-    mKeyboard(mWindow)
+    mKeyboard(mWindow),
+    mProjectionMatrix(matrix::GetProjection(ConvertDegreesToRadians(60.0f), 0.1f, 200.0f)),
+    mWater("Water", "WaterFactors", "Water", "WaterNormal"),
+    mCube("Default", "WaterDistortion", { 50.0f, -15.0f, -50.0f }, 20.0f),
+    mPostProcessor(std::bind(&Game::RenderWithPostProcessingEffect, this))
 {
     NAME_THREAD("Main");
     BENCHMARK;
-
     mWindow.SetCloseCallback(std::bind(&Game::CloseWindowCallback, std::ref(*this)));
+    mWindow.SetCursorCallback(std::bind(&Camera::UpdateRotation, std::ref(mCamera), 
+        std::placeholders::_1, std::placeholders::_2));
+
+    SetGlStates();
+
+    mPostProcessor.AddEffect("WaterEffect");
+    mPostProcessor.AddEffect("NoEffect");
+
+    // Start the timer
+    mTimer.Time();
 }
 
 Game::~Game()
 {
+    // Destructors should not throw exception, hence no GL macro
+    glDeleteVertexArrays(1, &mVao);
     SAVE_BENCHMARK;
 }
 
@@ -30,10 +47,12 @@ void Game::Loop()
 {
     BENCHMARK;
 
-    glClear(GL_COLOR_BUFFER_BIT);
+    GL(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
 
     Update();
     Render();
+
+    mDeltaTime = (float)mTimer.Time();
 
     NAMED_BENCHMARK("Swap and poll");
     // Swap front and back buffers
@@ -46,11 +65,39 @@ void Game::Loop()
 void Game::Update()
 {
     BENCHMARK;
+    // LOG(1.0f / mDeltaTime << std::endl);
+
+    mTime += (double)mDeltaTime;
+   
+    mCamera.UpdatePosition(mDeltaTime);
+    mWater.Update(mDeltaTime);
 }
 
-void Game::Render()
+void Game::Render() const
 {
     BENCHMARK;
+
+    // Render the scene with a water effect, if the
+    // camera is inside the water
+    mPostProcessor.Render(
+        mWater.IsPointInside(mCamera.GetPosition()) ? "WaterEffect" : "NoEffect");
+}
+
+void Game::RenderWithPostProcessingEffect()
+{
+    if (mWater.IsPointInside(mCamera.GetPosition()))
+    {
+        // If the camera is inside the water, render the cube
+        // without any distortions
+        mCube.Render(mCamera, mProjectionMatrix);
+    }
+    else
+    {
+        // If the camera is outside the water, render the cube
+        // with distortions
+        mCube.RenderWaterDistortion((float)mTime, mCamera, mProjectionMatrix);
+    }
+    mWater.Render((float)mTime, mCamera, mProjectionMatrix);
 }
 
 void Game::CloseWindowCallback()
@@ -59,3 +106,17 @@ void Game::CloseWindowCallback()
     mWindowShouldClose = true;
 }
 
+void Game::SetGlStates()
+{
+    // This vertex array object will not contain any states. 
+    // Its only purpose is to fulfill some GPUs requirement 
+    // to always have a vertex array object bound.
+    GL(glCreateVertexArrays(1, &mVao));
+    GL(glBindVertexArray(mVao));
+
+    GL(glClearColor(135.0f / 255.0f, 206.0f / 255.0f, 235.0f / 255.0f, 0.0f));
+    GL(glEnable(GL_DEPTH_TEST));
+    GL(glEnable(GL_CULL_FACE));
+    GL(glEnable(GL_BLEND));
+    GL(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+}
--- Water/Source/Game.h
+++ Water/Source/Game.h
@@ -1,6 +1,13 @@
 #pragma once
 #include "Window/Window.h"
 #include "Keyboard.h"
+#include "Rendering/Program.h"
+#include "Rendering/Camera.h"
+#include "Mathematics/Matrix/Matrix.h"
+#include "Timer.h"
+#include "Water.h"
+#include "Cube.h"
+#include "Rendering/PostProcessor.h"
 
 class Game
 {
@@ -12,10 +19,29 @@ public:
 private:
 	void Loop();
 	void Update();
-	void Render();
+	void Render() const;
+	void RenderWithPostProcessingEffect();
 	void CloseWindowCallback();
+	void SetGlStates();
 private:
 	Window mWindow;
 	Keyboard mKeyboard;
+	Camera mCamera;
 	bool mWindowShouldClose = false;
+
+	Timer mTimer;
+	// Make the delta time for the first frame
+	// 1/60 s, i.e., 60 FPS
+	float mDeltaTime = 1.0f / 60.0f;
+	double mTime = 0.0f;
+
+	// Some GPUs require that we have a vertex array 
+	// object bound and this vertex array object exists
+	// solely to fulfill that requirement
+	GLuint mVao = 0;
+
+	Matrix4 mProjectionMatrix;
+	Water mWater;
+	Cube mCube;
+	PostProcessor mPostProcessor;
 };
